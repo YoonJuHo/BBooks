f888723c02d7bf5b6c59b646615847763cf2c367
YoonJuHo <xoxoinsight@gmail.com>
2023-01-21 23:52:00 +0900

    [빌드] Create Project

---
+++ BBooks/BBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBooksApp.swift
+//  BBooks
+//
+//  Created by Bran MacBook Pro on 2023/01/21.
+//
+
+import SwiftUI
+
+@main
+struct BBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
